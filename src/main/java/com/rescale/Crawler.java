ddef9cfac601b8701bacc2727382a6b05207197c
Shivani Singhal <shivanisinghal22@gmail.com>
2017-09-09 18:30:27 -0700

    Initial commit of webCrawler

---
+++ src/main/java/com/rescale/Crawler.java
@@ -0,0 +1,180 @@
+package com.rescale;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.lang.StringBuilder;
+import java.util.concurrent.TimeUnit;
+import java.lang.InterruptedException;
+import org.jsoup.Connection;
+import java.util.HashSet;
+import java.util.ArrayList;
+
+public class Crawler{
+
+	private HashSet<String> visitedUrls;
+	private ArrayList<String> toVisitUrls;
+	private Integer count;
+	private final int MAX_THREADS = 100;
+
+	public Crawler(){
+		visitedUrls = new HashSet<String>();
+		toVisitUrls = new ArrayList<String>();
+		count = 0;
+	}
+
+	public static void main(String args[]) throws Exception{
+		if(args.length!=1){
+			System.out.println("Please provide the URL to be crawled as argument to program");
+			System.exit(0);
+		}
+	
+		Crawler webCrawler = new Crawler();
+		webCrawler.crawl(args[0]);
+	}
+
+	/*This function is the starting point to crawl through a url
+	 *It spaws a fixed threapool, the threads parallely crawl links 
+	 *accumulated in toVisitUrls list 
+	 * @param url : URL to be crawled 
+	 */
+	private void crawl(String url) throws Exception{
+		
+		int flag = 0;
+		toVisitUrls.add(url);
+		ThreadPoolExecutor executor = 
+			(ThreadPoolExecutor)Executors.newFixedThreadPool(MAX_THREADS);
+	    initiateThreads(executor);
+
+	    /*
+	     * Wait until all links are crawled then shutdown executor
+	     */
+	     
+	    while(flag == 0){
+		    synchronized(toVisitUrls){    
+		        if(!(count==0  && toVisitUrls.isEmpty())){
+		        	toVisitUrls.wait();
+		        }
+		        else{
+		        	flag = 1;
+		        	toVisitUrls.notifyAll();
+		        }
+		    }
+		}
+        executor.shutdownNow();
+	}
+
+	/*
+	 *initiates the threads in the threadpool
+	 *@param executor : ThreadPoolExecutor
+	 */
+	private void initiateThreads(ThreadPoolExecutor executor){
+
+		for(int i = 0; i < MAX_THREADS; i++){
+	            
+	        WorkerThread task = new WorkerThread();
+	        System.out.println("A new task has been added : " + i);
+	        executor.execute(task);
+	    }        
+	}
+
+	private class WorkerThread implements Runnable{
+	    private String url;
+	 
+	    public WorkerThread(){
+	        url = null;
+	    }
+	     
+	 	
+	 	/*This function picks urls from the toVisitUrl list in an infinite loop
+	 	 *and crawl each url to collect links
+	 	 *@param void
+	 	 */
+	    @Override
+	    public void run(){
+		
+	      outer:
+	    	while(true){
+				StringBuilder linkCollection = new StringBuilder();
+				url = null;
+				while(url == null){
+					synchronized(toVisitUrls){
+						if(toVisitUrls.isEmpty()){
+							try{
+								if(count!=0)
+									toVisitUrls.wait();
+								else{
+									toVisitUrls.notifyAll();
+									break outer;
+								}
+							}catch(InterruptedException e){
+								continue;
+							}
+						}
+						else{
+							url = toVisitUrls.remove(0);
+							visitedUrls.add(url);
+							count++;
+						}
+					}
+				}
+				
+				linkCollection.append(url);
+				collectUrls(linkCollection);
+
+				synchronized(toVisitUrls){
+					System.out.println("Out from here");
+					System.out.println(linkCollection.toString());
+					count--;
+					toVisitUrls.notifyAll();
+				}
+			}
+	    }
+
+
+	    /*This function connects to a webpage parses it for anchor tags
+	     *and collects all http or https links from it in toVisitUrl list
+		 * @param url : URL to be crawled 
+	 	 */
+	    private void collectUrls(StringBuilder linkCollection){
+	    	Document doc = null;
+			try{
+				Connection connection = Jsoup.connect(url);
+
+				//connection.userAgent("Mozilla/5.0");
+				//set timeout to 10 seconds
+    			connection.timeout(10 * 10000);
+    			doc = connection.get();
+			}catch(Exception e){
+				e.printStackTrace();
+				return;	
+			}
+		//	System.out.println("starting to collecr urls");
+			Elements links = doc.getElementsByTag("a");
+			for (Element link : links) {
+				//System.out.println("In the loop");
+				//System.out.println(link.attr("href"));
+				String linkHref = link.attr("href");
+				if((linkHref.startsWith("http://")|| 
+					linkHref.startsWith("https://")) && linkHref.contains("rescale.com/")){
+					
+				 //  System.out.println("got one url");
+					
+					linkCollection.append("\n ");
+					linkCollection.append(linkHref);
+					synchronized(toVisitUrls){
+						if(!(visitedUrls.contains(linkHref) || 
+							toVisitUrls.contains(linkHref))){
+							toVisitUrls.add(linkHref);
+							toVisitUrls.notifyAll();
+						}
+					}
+					//System.out.println(linkCollection.toString());
+				}
+			}
+	    }
+
+	}
+}
